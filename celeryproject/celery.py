f89460346eaafc30735aab5a1fed38e275e0ab08
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-07-17 13:38:51 +0530

    Create simple add function

--- celeryproject/celery.py
+++ celeryproject/celery.py
@@ -1,5 +1,5 @@
 import os
-
+from time import sleep
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
@@ -17,6 +17,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-# @app.task(bind=True, ignore_result=True)
-# def debug_task(self):
-#     print(f'Request: {self.request!r}')
+@app.task
+def add(x,y):
+    sleep(10)
+    return x+y
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from celeryproject.celery import add
+from time import sleep
 # Create your views here.
+
+
+# Enqueue task using delay()
+def home(request):
+    result= add.delay(18,12)  # it enqueue task (send task for execution )
+    print(result)
+    return render(request,'index.html')
