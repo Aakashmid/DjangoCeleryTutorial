f78535be13c10ac35b9c2524b86258eaa4028f13
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-07-27 13:20:41 +0530

    Modified login signup page

--- EmailSender/App/views.py
+++ EmailSender/App/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.decorators import login_required
+# from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+# from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+# from allauth.socialaccount.helpers import complete_social_login
+from allauth.socialaccount.models import SocialApp
+
 
 # Create your views here.
 
 # @login_required  #   it redirect to login url if user is unauthenticated
 def index(request):
     return render(request,'App/index.html')
+
+
+# Authenticated related views
+from django.shortcuts import redirect
+import urllib.parse
+
+# def custom_google_login(request):
+#     # Construct the Google OAuth2 authorization URL manually
+#     base_url = "https://accounts.google.com/o/oauth2/v2/auth"
+#     params = {
+#         'response_type': 'code',
+#         'client_id': SocialApp.client_id,
+#         'redirect_uri': request.build_absolute_uri('/accounts/google/login/callback/'),
+#         'scope': 'profile email',
+#         'response_type':'code',
+#         "state": "2dq7rIOnrtfnA8Eg",
+#         'access_type': 'offline',
+#     }
+#     auth_url = f"{base_url}?{urllib.parse.urlencode(params)}"
+    
+#     return redirect(auth_url)
+# def direct_google_login(request):
+#     app=SocialApp.objects.get(provider='google')
+#     callback_url=request.build_absolute_uri('/accounts/google/login/callback/')
+#     access_token_url='https://oauth2.googleapis.com/token'
+#     client=OAuth2Client(request,app.client_id,app.secret,app.key,access_token_url=access_token_url,callback_url=callback_url)
+#     adapter=GoogleOAuth2Adapter(request)
+#     provider=adapter.get_provider()
+#     authorize_url=provider.get_authorize_url(request,callback_url,client)
+#     return redirect(authorize_url)
+
+
+
+
--- EmailSender/Project/urls.py
+++ EmailSender/Project/urls.py
@@ -1,9 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),  
     path('',include('App.urls')),
-]
+    path('accounts/', include('allauth.urls')),  
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # not recommended in production
+
+# for developent only 
+# if settings.DEBUG:
+#     urlpatterns=+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
